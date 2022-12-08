9bd411ebdc25696a2726355ba26f4a0ff55485c6
bomky <bom_ky@outlook.com>
2022-12-08 17:29:31 +0800

    移除rsw

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteRsw } from 'vite-plugin-rsw'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteRsw()]
+  plugins: [react(),]
 })
