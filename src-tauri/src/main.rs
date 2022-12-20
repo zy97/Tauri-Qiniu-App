346ad96d441b7f32176e91c956949f7f523d100e
bomky <bom_ky@outlook.com>
2022-12-20 11:04:57 +0800

    添加托管

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,9 +6,47 @@ extern crate humansize;
 mod commands;
 mod models;
 use commands::qn_command::test;
+use tauri::{
+    generate_context, CustomMenuItem, Manager, RunEvent, SystemTray, SystemTrayEvent,
+    SystemTrayMenu, SystemTrayMenuItem, WindowBuilder,
+};
 fn main() {
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+    let tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| match event {
+            SystemTrayEvent::DoubleClick { .. } => {
+                WindowBuilder::new(app, "main", tauri::WindowUrl::App("index.html".into()))
+                    .build()
+                    .unwrap();
+
+                // app.get_window("main").unwrap().show();
+            }
+            SystemTrayEvent::MenuItemClick { id, .. } => match id.as_str() {
+                "quit" => {
+                    std::process::exit(0);
+                }
+                "hide" => {
+                    let window = app.get_window("main").unwrap();
+                    window.hide().unwrap();
+                }
+                _ => {}
+            },
+            _ => {}
+        })
         .invoke_handler(tauri::generate_handler![test])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(generate_context!())
+        .expect("error while building tauri application")
+        .run(|app, event| match event {
+            RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
